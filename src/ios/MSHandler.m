stephanebachelier/moodstocks-phonegap-plugin

commit b8c6f32acfe606180c449dada705efd2ebd4f2bc

iOS: adapt plugin for ARC/non-ARC setting [fix #6]

src/ios/MSHandler.m
@@ -42,7 +42,9 @@
 }
 
 - (void)dealloc {
+#if !__has_feature(objc_arc)
     [super dealloc];
+#endif
 
     self.plugin = nil;
     self.callback = nil;
@@ -53,7 +55,9 @@
     MSScanner *scanner = [MSScanner sharedInstance];
     if ([scanner isSyncing]) return;
     // Retain until the sync is finished (see below)
+#if !__has_feature(objc_arc)
     [self retain];
+#endif
     [scanner syncWithDelegate:self];
 #endif
 }
@@ -84,8 +88,9 @@
                          progress:100
                          callback:self.callback
                shouldKeepCallback:NO];
-
+#if !__has_feature(objc_arc)
     [self release];
+#endif
 }
 
 - (void)scanner:(MSScanner *)scanner failedToSyncWithError:(NSError *)error {
@@ -98,8 +103,9 @@
                              callback:self.callback
                    shouldKeepCallback:YES];
     }
-
+#if !__has_feature(objc_arc)
     [self release];
+#endif
 }
 
 #pragma mark - Scanner Session Handler

src/ios/MoodstocksPlugin.m
@@ -84,8 +84,9 @@
     // NOTE: will be released when sync is over (please refer to MSHandler.m)
     MSHandler *syncHandler = [[MSHandler alloc] initWithPlugin:self callback:command.callbackId];
     [syncHandler sync];
-
+#if !__has_feature(objc_arc)
     [syncHandler release];
+#endif
 }
 
 // Plugin method - scan: set scan options & launch the scanner
@@ -107,8 +108,9 @@
                                                          plugin:self];
 
     [self.viewController presentModalViewController:self.scanner animated:YES];
-
+#if !__has_feature(objc_arc)
     [scanHandler release];
+#endif
 }
 
 // Plugin method - pause: pause the scanner session
@@ -145,8 +147,11 @@
 - (void)updateScanResult:(NSString *)value
                   format:(int)format
                 callback:(NSString *)callback {
-
+#if __has_feature(objc_arc)
+    NSMutableDictionary *resultDict = [[NSMutableDictionary alloc] init];
+#else
     NSMutableDictionary *resultDict = [[[NSMutableDictionary alloc] init] autorelease];
+#endif
 
     // Scan result format
     [resultDict setObject:[NSNumber numberWithInteger:format] forKey:@"format"];

plugin.xml
@@ -31,11 +31,11 @@
 
         <!-- header & source files -->
         <header-file src="src/ios/MoodstocksPlugin.h" />
-        <source-file src="src/ios/MoodstocksPlugin.m" compiler-flags="-fno-objc-arc" />
+        <source-file src="src/ios/MoodstocksPlugin.m" />
         <header-file src="src/ios/MSHandler.h" />
-        <source-file src="src/ios/MSHandler.m" compiler-flags="-fno-objc-arc" />
+        <source-file src="src/ios/MSHandler.m" />
         <header-file src="src/ios/MSScannerController.h" />
-        <source-file src="src/ios/MSScannerController.m" compiler-flags="-fno-objc-arc" />
+        <source-file src="src/ios/MSScannerController.m" />
         <header-file src="src/ios/MoodstocksAPI.h" />
 
         <!-- system frameworks -->

src/ios/MSScannerController.m
@@ -60,9 +60,10 @@
 }
 
 - (void)dealloc {
+#if !__has_feature(objc_arc)
     [super dealloc];
-
     [_scannerSession release];
+#endif
     self.result = nil;
     self.handler = nil;
 }
@@ -71,7 +72,11 @@
     [super loadView];
 
     CGRect scannerFrame = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds));
+#if __has_feature(objc_arc)
+    _videoPreview = [[UIView alloc] initWithFrame:scannerFrame];
+#else
     _videoPreview = [[[UIView alloc] initWithFrame:scannerFrame] autorelease];
+#endif
     _videoPreview.backgroundColor = [UIColor blackColor];
     _videoPreview.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     _videoPreview.autoresizesSubviews = YES;
@@ -101,20 +106,38 @@
 
     [videoPreviewLayer insertSublayer:captureLayer below:[[videoPreviewLayer sublayers] objectAtIndex:0]];
 
+#if __has_feature(objc_arc)
+    _toolbar = [[UIToolbar alloc] init];
+#else
     _toolbar = [[[UIToolbar alloc] init] autorelease];
+#endif
     _toolbar.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
     _toolbar.barStyle = UIBarStyleBlack;
     _toolbar.tintColor = nil;
 
+#if __has_feature(objc_arc)
+    _barButton = [[UIBarButtonItem alloc]
+                   initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
+                   target:self
+                   action:@selector(dismissAction)];
+#else
     _barButton = [[[UIBarButtonItem alloc]
                    initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
                    target:self
                    action:@selector(dismissAction)] autorelease];
+#endif
 
+#if __has_feature(objc_arc)
+    id flexSpace = [[UIBarButtonItem alloc]
+                    initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                    target:nil
+                    action:nil];
+#else
     id flexSpace = [[[UIBarButtonItem alloc]
                     initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
                     target:nil
                     action:nil] autorelease];
+#endif
 
     _toolbar.items = [NSArray arrayWithObjects:_barButton,flexSpace,nil];
     [_toolbar sizeToFit];

commit 77c566939ad4d835db344d230673fbbff3e0bf4e

iOS: Fix memory leak & regression in issue #6
This also fix the regression caused by the commit b8c6f32acf that closes 
issue #6. The regression is showed as `sync` method no longer receive
the callback messages.

The solution involves using CFRetain & CFRelease to release the plugin's `self` object. It's also important to set syncHandler & scanHandler as
`nil` otherwise it will cause a memory leak.

src/ios/MSHandler.m
@@ -45,7 +45,6 @@
 #if !__has_feature(objc_arc)
     [super dealloc];
 #endif
-
     self.plugin = nil;
     self.callback = nil;
 }
@@ -55,7 +54,9 @@
     MSScanner *scanner = [MSScanner sharedInstance];
     if ([scanner isSyncing]) return;
     // Retain until the sync is finished (see below)
-#if !__has_feature(objc_arc)
+#if __has_feature(objc_arc)
+    CFRetain((__bridge CFTypeRef)(self));
+#else
     [self retain];
 #endif
     [scanner syncWithDelegate:self];
@@ -88,7 +89,9 @@
                          progress:100
                          callback:self.callback
                shouldKeepCallback:NO];
-#if !__has_feature(objc_arc)
+#if __has_feature(objc_arc)
+    CFRelease((__bridge const void*)self);
+#else
     [self release];
 #endif
 }
@@ -103,7 +106,9 @@
                              callback:self.callback
                    shouldKeepCallback:YES];
     }
-#if !__has_feature(objc_arc)
+#if __has_feature(objc_arc)
+    CFRelease((__bridge const void*)self);
+#else
     [self release];
 #endif
 }

src/ios/MoodstocksPlugin.m
@@ -84,7 +84,10 @@
     // NOTE: will be released when sync is over (please refer to MSHandler.m)
     MSHandler *syncHandler = [[MSHandler alloc] initWithPlugin:self callback:command.callbackId];
     [syncHandler sync];
-#if !__has_feature(objc_arc)
+
+#if __has_feature(objc_arc)
+    syncHandler = nil;
+#else
     [syncHandler release];
 #endif
 }
@@ -108,7 +111,10 @@
                                                          plugin:self];
 
     [self.viewController presentModalViewController:self.scanner animated:YES];
-#if !__has_feature(objc_arc)
+
+#if __has_feature(objc_arc)
+    scanHandler = nil;
+#else
     [scanHandler release];
 #endif
 }